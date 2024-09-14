1a7e4e483c72d5ecc8afb439e89590bd8ade7d21
Quinn Semele <quinn@compasses.dev>
2024-09-14 17:51:54 +0100

    I don't need GSON field now.

--- common/src/main/java/dev/compasses/reasonable_defaults/Constants.java
+++ common/src/main/java/dev/compasses/reasonable_defaults/Constants.java
@@ -1,16 +1,9 @@
 package dev.compasses.reasonable_defaults;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Constants {
 	public static final String MOD_NAME = "Reasonable Defaults";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
-	public static final Gson GSON = new GsonBuilder()
-			.disableHtmlEscaping()
-			.setPrettyPrinting()
-			.create();
-
 }
